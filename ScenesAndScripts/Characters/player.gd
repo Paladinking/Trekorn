dd5176ae2610b19c5edc784054f90fb241e2340e
Filip Berg <filbe316@student.liu.se>
2024-07-06 14:34:54 +0200

    big camera improvements

--- ScenesAndScripts/Characters/player.gd
+++ ScenesAndScripts/Characters/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 6.0
+const SPRINT_SPEED_MULT = 2
 const SLOW_DOWN_MULT = 0.5
 const ACCELERATION = 25.0
 const ACCELERATION_IN_AIR_MULT = 0.1
@@ -9,14 +10,15 @@ const JUMP_VELOCITY = 5.0
 const MAX_JUMP_TIME = 0.25
 var jumping = 0.0
 
-const CAMERA_MOVE_SPEED = 5
+#const CAMERA_MOVE_SPEED = 5
+const CAMERA_MOVE_SPEED = 2.5
 const CAMERA_MAX_DISTANCE = 10
 var camera_target_position = Vector3(0, 1.5, 0)
 const CAMERA_WALL_SAFETY_DIST = 1
 var camera_angle_y = 0
-var camera_angle_x = 0
-const CAMERA_ANGLE_X_MAX = deg_to_rad(90)
-const CAMERA_ANGLE_X_MIN = deg_to_rad(-15)
+var camera_angle_x = deg_to_rad(90)
+const CAMERA_ANGLE_X_MAX = deg_to_rad(179)
+const CAMERA_ANGLE_X_MIN = deg_to_rad(45)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -38,12 +40,17 @@ func _physics_process(delta):
 	#if is_on_floor():
 	var input_dir = Input.get_vector("go_left", "go_right", "go_forward", "go_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).rotated(Vector3.UP, camera_angle_y)
-	if Input.is_action_pressed("slow"):
-		direction *= SLOW_DOWN_MULT
-	var current_acc = ACCELERATION
+	var current_acceleration = ACCELERATION
 	if not is_on_floor():
-		current_acc *= ACCELERATION_IN_AIR_MULT
-	velocity = velocity.move_toward(direction * SPEED, delta * current_acc)
+		current_acceleration *= ACCELERATION_IN_AIR_MULT
+	var current_speed = SPEED
+	if Input.is_action_pressed("sprint"):
+		current_speed *= SPRINT_SPEED_MULT
+	elif Input.is_action_pressed("slow"):
+		current_speed *= SLOW_DOWN_MULT
+	velocity = velocity.move_toward(direction * current_speed, delta * current_acceleration)
+	if not direction.is_zero_approx():
+		$Model.rotation.y = -Vector3(velocity.x, 0, velocity.z).signed_angle_to(Vector3.RIGHT, Vector3.UP)
 
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
@@ -60,6 +67,7 @@ func _physics_process(delta):
 				jump_direction = direction
 			velocity += jump_direction * JUMP_VELOCITY
 			velocity.y = JUMP_VELOCITY
+			$Model.rotation.y = -Vector3(velocity.x, 0, velocity.z).signed_angle_to(Vector3.RIGHT, Vector3.UP)
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -74,7 +82,6 @@ func _physics_process(delta):
 		else:
 			jumping = 0.0
 
-	$Model.rotation.y = -Vector3(velocity.x, 0, velocity.z).signed_angle_to(Vector3.RIGHT, Vector3.UP)
 	#if velocity.is_zero_approx():
 		#$Model/AnimationPlayer.play("Idle")
 
@@ -83,33 +90,34 @@ func _physics_process(delta):
 
 func _process(delta):
 	if Input.is_action_pressed("look_up") or Input.is_action_pressed("look_down") or \
-		Input.is_action_pressed("look_left") or Input.is_action_pressed("look_right"):
-		camera_angle_y += (Input.get_action_strength("look_left") - Input.get_action_strength("look_right")) * delta * CAMERA_MOVE_SPEED
+		Input.is_action_pressed("look_right") or Input.is_action_pressed("look_left"):
+		camera_angle_y -= (Input.get_action_strength("look_right") - Input.get_action_strength("look_left")) * delta * CAMERA_MOVE_SPEED
 		if camera_angle_y > 2 * PI:
 			camera_angle_y -= 2 * PI
 		elif camera_angle_y < -2 * PI:
 			camera_angle_y += 2 * PI
 
-		camera_angle_x += (Input.get_action_strength("look_down") - Input.get_action_strength("look_up")) * delta * CAMERA_MOVE_SPEED
+		camera_angle_x += (Input.get_action_strength("look_up") - Input.get_action_strength("look_down")) * delta * CAMERA_MOVE_SPEED
 		camera_angle_x = clamp(camera_angle_x, CAMERA_ANGLE_X_MIN, CAMERA_ANGLE_X_MAX)
 
 	#var rot_y = CAMERA_POSITION.rotated(Vector3.UP, camera_angle_y)
 	#$Camera.position = rot_y.rotated(Vector3.UP.cross(rot_y).normalized(), camera_angle_x)
-	$Camera.position = Vector3(sin(camera_angle_y), camera_angle_x, cos(camera_angle_y)).normalized() * CAMERA_MAX_DISTANCE
+	var camera_position = Vector3(
+		sin(camera_angle_x) * sin(camera_angle_y),
+		-cos(camera_angle_x),
+		sin(camera_angle_x) * cos(camera_angle_y)
+	) * CAMERA_MAX_DISTANCE
+	$Camera.position = camera_position
 	$Camera.look_at(global_position + camera_target_position)
-	$CameraRay.target_position = $Camera.position
+	$CameraRay.target_position = camera_position
 
 	if $CameraRay.is_colliding():
-		#print("Cam: ", $Camera.position)
-		#print("Col: ", $CameraRay.get_collision_point() - $CameraRay.global_position)
-		var camera_ray_collision_distance_squared = $CameraRay.position.distance_squared_to($CameraRay.get_collision_point() - $CameraRay.global_position)
-		#if camera_ray_collision_distance_squared < (CAMERA_MAX_DISTANCE + CAMERA_WALL_SAFETY_DIST) * (CAMERA_MAX_DISTANCE + CAMERA_WALL_SAFETY_DIST):
-		if camera_ray_collision_distance_squared < CAMERA_MAX_DISTANCE * CAMERA_MAX_DISTANCE:
-			var to_move_camera = CAMERA_MAX_DISTANCE - sqrt(camera_ray_collision_distance_squared)
-			#to_move_camera += CAMERA_WALL_SAFETY_DIST / (CAMERA_MAX_DISTANCE - (to_move_camera + CAMERA_WALL_SAFETY_DIST))
+		var collision_point = $CameraRay.get_collision_point() - $CameraRay.global_position + $CameraRay.position
+		var camera_ray_collision_distance = $CameraRay.position.distance_to($CameraRay.get_collision_point() - $CameraRay.global_position + $CameraRay.position)
+		if camera_ray_collision_distance < CAMERA_MAX_DISTANCE:
+			var to_move_camera = CAMERA_MAX_DISTANCE - camera_ray_collision_distance
+			to_move_camera += CAMERA_WALL_SAFETY_DIST * (1 - (CAMERA_MAX_DISTANCE - (to_move_camera + CAMERA_WALL_SAFETY_DIST)) / CAMERA_MAX_DISTANCE)
 			$Camera.position = $Camera.position.move_toward(camera_target_position, to_move_camera)
-			#if camera_angle_x < 0:
-				#$Camera.position.y += ($CameraRay.get_collision_point().y - $CameraRay.global_position.y) + CAMERA_WALL_SAFETY_DIST
 
 			if CAMERA_MAX_DISTANCE - to_move_camera < CAMERA_WALL_SAFETY_DIST:
 				$Model.hide()
@@ -118,3 +126,10 @@ func _process(delta):
 	elif not $Model.is_visible_in_tree():
 		$Model.show()
 
+#var p = false
+
+func _unhandled_key_input(event):
+	#if event.is_pressed() and event.physical_keycode == KEY_P:
+		#p = !p
+		#print(p)
+	pass
