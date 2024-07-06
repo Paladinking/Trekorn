2d99ba60f0efe04c9c0c282eed9e49bc73d5f6ba
Filip Berg <filbe316@student.liu.se>
2024-07-06 11:39:13 +0200

    player stuff

--- ScenesAndScripts/Characters/player.gd
+++ ScenesAndScripts/Characters/player.gd
@@ -10,20 +10,25 @@ const MAX_JUMP_TIME = 0.25
 var jumping = 0.0
 
 const CAMERA_MOVE_SPEED = 5
-const CAMERA_POSITION = Vector3(0, 3, 7)
+const CAMERA_MAX_DISTANCE = 10
+var camera_target_position = Vector3(0, 1.5, 0)
+const CAMERA_WALL_SAFETY_DIST = 1
 var camera_angle_y = 0
 var camera_angle_x = 0
-const CAMERA_ANGLE_X_MAX = deg_to_rad(15)
-const CAMERA_ANGLE_X_MIN = deg_to_rad(-40)
+const CAMERA_ANGLE_X_MAX = deg_to_rad(90)
+const CAMERA_ANGLE_X_MIN = deg_to_rad(-15)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _ready():
-	var rot_y = CAMERA_POSITION.rotated(Vector3.UP, camera_angle_y)
-	$Camera.position = rot_y.rotated(Vector3.UP.cross(rot_y).normalized(), camera_angle_x)
-	$Camera.look_at(global_position + Vector3(0, 1, 0))
+	#var rot_y = CAMERA_POSITION.rotated(Vector3.UP, camera_angle_y)
+	#$Camera.position = rot_y.rotated(Vector3.UP.cross(rot_y).normalized(), camera_angle_x)
+	camera_target_position = $CameraRay.position
+	$Camera.position = Vector3(sin(camera_angle_y), camera_angle_x, cos(camera_angle_y)) * CAMERA_MAX_DISTANCE
+	$Camera.look_at(global_position + camera_target_position)
+	$CameraRay.target_position = $Camera.position
 
 
 func _physics_process(delta):
@@ -46,7 +51,7 @@ func _physics_process(delta):
 			velocity.y = JUMP_VELOCITY
 
 		elif is_on_wall_only():
-			if Input.is_action_just_pressed("jump"):
+			if Input.is_action_pressed("jump"):
 				jumping = MAX_JUMP_TIME
 				var collision : KinematicCollision3D = get_last_slide_collision()
 				var jump_direction = collision.get_position().direction_to(global_position)
@@ -56,7 +61,6 @@ func _physics_process(delta):
 					jump_direction = direction
 				velocity += jump_direction * JUMP_VELOCITY
 				velocity.y = JUMP_VELOCITY
-				#$Model.rotation.y = -velocity.signed_angle_to(Vector3.RIGHT, Vector3.UP)
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -71,6 +75,7 @@ func _physics_process(delta):
 		else:
 			jumping = 0.0
 
+	$Model.rotation.y = -velocity.signed_angle_to(Vector3.RIGHT, Vector3.UP)
 	#if velocity.is_zero_approx():
 		#$Model/AnimationPlayer.play("Idle")
 
@@ -86,10 +91,31 @@ func _process(delta):
 		elif camera_angle_y < -2 * PI:
 			camera_angle_y += 2 * PI
 
-		camera_angle_x += (Input.get_action_strength("look_up") - Input.get_action_strength("look_down")) * delta * CAMERA_MOVE_SPEED
+		camera_angle_x += (Input.get_action_strength("look_down") - Input.get_action_strength("look_up")) * delta * CAMERA_MOVE_SPEED
 		camera_angle_x = clamp(camera_angle_x, CAMERA_ANGLE_X_MIN, CAMERA_ANGLE_X_MAX)
 
-		var rot_y = CAMERA_POSITION.rotated(Vector3.UP, camera_angle_y)
-		$Camera.position = rot_y.rotated(Vector3.UP.cross(rot_y).normalized(), camera_angle_x)
-		$Camera.look_at(global_position + Vector3(0, 1, 0))
+	#var rot_y = CAMERA_POSITION.rotated(Vector3.UP, camera_angle_y)
+	#$Camera.position = rot_y.rotated(Vector3.UP.cross(rot_y).normalized(), camera_angle_x)
+	$Camera.position = Vector3(sin(camera_angle_y), camera_angle_x, cos(camera_angle_y)).normalized() * CAMERA_MAX_DISTANCE
+	$Camera.look_at(global_position + camera_target_position)
+	$CameraRay.target_position = $Camera.position
+
+	if $CameraRay.is_colliding():
+		#print("Cam: ", $Camera.position)
+		#print("Col: ", $CameraRay.get_collision_point() - $CameraRay.global_position)
+		var camera_ray_collision_distance_squared = $CameraRay.position.distance_squared_to($CameraRay.get_collision_point() - $CameraRay.global_position)
+		#if camera_ray_collision_distance_squared < (CAMERA_MAX_DISTANCE + CAMERA_WALL_SAFETY_DIST) * (CAMERA_MAX_DISTANCE + CAMERA_WALL_SAFETY_DIST):
+		if camera_ray_collision_distance_squared < CAMERA_MAX_DISTANCE * CAMERA_MAX_DISTANCE:
+			var to_move_camera = CAMERA_MAX_DISTANCE - sqrt(camera_ray_collision_distance_squared)
+			#to_move_camera += CAMERA_WALL_SAFETY_DIST / (CAMERA_MAX_DISTANCE - (to_move_camera + CAMERA_WALL_SAFETY_DIST))
+			$Camera.position = $Camera.position.move_toward(camera_target_position, to_move_camera)
+			#if camera_angle_x < 0:
+				#$Camera.position.y += ($CameraRay.get_collision_point().y - $CameraRay.global_position.y) + CAMERA_WALL_SAFETY_DIST
+
+			if CAMERA_MAX_DISTANCE - to_move_camera < CAMERA_WALL_SAFETY_DIST:
+				$Model.hide()
+			elif not $Model.is_visible_in_tree():
+				$Model.show()
+	elif not $Model.is_visible_in_tree():
+		$Model.show()
 
--- ScenesAndScripts/main.gd
+++ ScenesAndScripts/main.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _unhandled_key_input(event):
+	if event.is_pressed() and event.physical_keycode == KEY_F:
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
