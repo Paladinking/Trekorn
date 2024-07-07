16bd3c724d4f431178499a756fa7bfdf04f6ab2d
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2024-07-07 16:53:58 +0200

    snow yime

--- ScenesAndScripts/Environments/Snow/snow.gd
+++ ScenesAndScripts/Environments/Snow/snow.gd
@@ -1,31 +1,15 @@
-extends GPUParticles3D
+extends Node3D
 
 #@onready var body: StaticBody3D = $StaticBody3D
 var body;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#print("DÅ")
-	body = get_parent().find_child("StaticBody3D");
+	body = get_parent().find_child("Player");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	self.position = Vector3(body.global_transform.origin.x, body.global_transform.origin.y +5, body.global_transform.origin.z)
+	
 
-	print("Hej")
-	var pos = body.global_transform.origin
-	#print(pos);
-	#set_instance_shader_parameter("player_pos", pos);
-	#self.material_override.set_shader_parameter("player_pos", pos);
-	#draw passes shader
-	
-	#print(self.material_override.get_shader_parameter("player_pos"));
-	
-	self.position = pos;
-	
-	#self.process_material.set_shader_parameter("wind_dir", pos);
-	#draw passes shader
-	
-	#print(self.process_material.get_shader_parameter("wind_dir"));
-	
-	#self.position = pos;
 	
 
