24638a8642a5f098adb71d87e0d438b25973e857
Axel <axel.hammarberg@gmail.com>
2024-07-07 18:49:11 +0200

    No more jumping

--- ScenesAndScripts/Characters/targt.gd
+++ ScenesAndScripts/Characters/targt.gd
@@ -18,10 +18,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	velocity.x = move_toward(velocity.x, 0, SPEED)
 	velocity.z = move_toward(velocity.z, 0, SPEED)
 
