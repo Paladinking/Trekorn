eaa9cccc263a5043050a96aa92475c300ea7121e
Axel <axel.hammarberg@gmail.com>
2024-07-07 16:02:44 +0200

    Started on target, recoil

---
+++ ScenesAndScripts/Characters/targt.gd
@@ -0,0 +1,24 @@
+class_name Target
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
